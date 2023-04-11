4242d70855f95ecf99ddb4d70b75f1ed5f93aff4
devynj4722 <jmoneyaae@gmail.com>
2023-04-11 13:47:20 -0700

    implement login and register pages

--- server/server.js
+++ server/server.js
@@ -7,20 +7,26 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 const Lab = require('./models/schemas')
-
+const User = require('./models/schemas')
 
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json()); 
-
-app.use(bodyParser.json());
+// Middle Wares
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+
 app.use(express.static(__dirname + '/public'))
 const dbstring = "mongodb+srv://jmoneyaae:8dr81uz5SnWZxpWH@dbstorage.hqut4f2.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(dbstring, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
 
+// Define Passport strategy
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 
@@ -28,6 +34,18 @@ app.post('/lab', (req, res)=>{
   try {
    const {Host, address, date} = req.body
    res.json({host: Host, address: address, date:date})
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.post('/register', (req, res)=>{
+  try {
+    const {user, password, email, confirmPassword} = req.body
+    if (password !== confirmPassword) {
+      throw new Error("Passwords do not match");
+    }
+    res.json({username: user, password: password, email: email})
   } catch (error) {
     console.log(error)
   }
